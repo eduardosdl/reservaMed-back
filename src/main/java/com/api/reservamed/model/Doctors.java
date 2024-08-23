a7f311266545de1ba2093b34729e65c1bbd55401
Dyeggo Silva <dyeggo0102@live.com>
2024-08-23 01:05:26 -0300

    :sparkles: feat - criação de atributos Doctors

--- src/main/java/com/api/reservamed/model/Doctors.java
+++ src/main/java/com/api/reservamed/model/Doctors.java
@@ -1,5 +1,24 @@
 package com.api.reservamed.model;
 
-public class Doctors {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctors {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column (unique = true, nullable = false)
+    private String crm;
+    private String specialty;
+    private String cellPhone;
+    
 }
