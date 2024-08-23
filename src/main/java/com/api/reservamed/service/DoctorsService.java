d2795fd6399e92f68d9b1818366505ad01b29523
Dyeggo Silva <dyeggo0102@live.com>
2024-08-23 01:21:53 -0300

    :sparkles: feat - criação dos métodos do DoctorsService

--- src/main/java/com/api/reservamed/service/DoctorsService.java
+++ src/main/java/com/api/reservamed/service/DoctorsService.java
@@ -1,5 +1,41 @@
 package com.api.reservamed.service;
 
+import com.api.reservamed.model.Doctors;
+import com.api.reservamed.repositories.DoctorsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class DoctorsService {}
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorsService {
+    @Autowired
+    DoctorsRepository doctorsRepository;
+
+    public List<Doctors> listAll(){
+        return doctorsRepository.findAll();
+    }
+
+    public Object listDoctorCrm(String crm){
+        return doctorsRepository.findByCrm(crm);
+    }
+
+    public Doctors saveDoctor(Doctors doctor){
+        return doctorsRepository.save(doctor);
+    }
+
+    public void dellDoctor(String crm) {
+        doctorsRepository.deleteByCrm(crm);
+    }
+
+    public Optional<Doctors> updateDoctor(String crm, Doctors doctor) {
+        Optional<Doctors> doctorCrm = Optional.ofNullable(doctorsRepository.findByCrm(crm));
+        doctorCrm = Optional.of(doctorsRepository.save(doctor));
+        return doctorCrm;
+    }
+
+
+}
+
 
