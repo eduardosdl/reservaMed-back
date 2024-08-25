6d20ec4fe8e27d534e09e86c93163628de477ad2
Matheus Moreira <micaelmodesto11@gmail.com>
2024-08-25 18:44:41 -0300

    FEATURE - Adicionando tratador de erros

--- src/main/java/com/api/reservamed/infra/exception/TratadorDeErros.java
+++ src/main/java/com/api/reservamed/infra/exception/TratadorDeErros.java
@@ -1,4 +1,40 @@
 package com.api.reservamed.infra.exception;
 
+import com.api.reservamed.infra.ValidacaoException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public ResponseEntity tratarError401(){
+        return ResponseEntity.badRequest().body("Seu usuário não é permitido para acessar esta rota!");
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity tratarRegraDeNegocio(ValidacaoException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
