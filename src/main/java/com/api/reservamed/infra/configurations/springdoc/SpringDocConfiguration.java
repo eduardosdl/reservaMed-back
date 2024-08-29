7665b7c2295e0d397746678de97e4022c02493dd
Matheus Moreira <micaelmodesto11@gmail.com>
2024-08-28 22:33:06 -0300

    FEATURE - Adicionando swagger

---
+++ src/main/java/com/api/reservamed/infra/configurations/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,14 @@
+package com.api.reservamed.infra.configurations.springdoc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
+}
